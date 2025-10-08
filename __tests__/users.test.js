b0959d979f33a2536483186857e88abbc459b931
antonovmike <antonovma@outlook.com>
2025-10-08 09:12:49 +0200

    [IMP] users.test.js - checking that the res.body object contains a property named token

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -43,6 +43,7 @@ describe("User routes", () => {
 
     expect(res.statusCode).toBe(200);
     // Write a test to verify the token
+    expect(res.body).toHaveProperty("token");
   });
 
   test("POST /users/login fails with invalid password", async () => {
