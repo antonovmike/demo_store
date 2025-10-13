38fe9fb4089c6f0d85e3f3e6e293dc53dda3cf71
antonovmike <antonovma@outlook.com>
2025-10-13 11:42:19 +0200

    [CONF] Add environment variables to database configuration
    This commit adds environment variables for the database configuration in the config/config.js file. The development, test, and production environments are configured to use the environment variables for the username, password, database, host, and port. The dialect is set to "postgres" for all environments.
    
    This allows for easy configuration of the database connection settings without hardcoding them in the codebase.

--- config/config.js
+++ config/config.js
@@ -1,3 +1,28 @@
 require("dotenv").config();
 
-module.exports = {};
+module.exports = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE_TEST || process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: "postgres"
+  }
+};
