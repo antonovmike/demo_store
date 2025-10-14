dd5ab298030131e19eba55e517c5a945bc6bb295
antonovmike <antonovma@outlook.com>
2025-10-14 08:17:57 +0200

    [ADD] Add file productsRoutes.js.
    This commit adds a new file, routes/productsRoutes.js, which serves as a template for defining the routes for managing products in the application. The file includes placeholders only.

---
+++ routes/productsRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { Product } = require("../models");
+
+const router = express.Router();
+
+// GET /products
+
+// GET /products/:id
+
+// POST /products
+
+// PUT /products/:id
+
+// DELETE /products/:id
+
+module.exports = router;
