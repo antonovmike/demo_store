e72f4801997f3f0afb7ed23c820307053763eee1
antonovmike <antonovma@outlook.com>
2025-10-20 15:39:46 +0200

    [IMP] Added imports to ProfilePage.jsx

---
+++ demo-store-client/src/components/ProfilePage.jsx
@@ -0,0 +1,5 @@
+import React, { useContext, useEffect, useState } from 'react';
+import api from "../api/axios";
+import { AuthContext } from '../context/AuthContext';
+
+export default function ProfilePage() {}
