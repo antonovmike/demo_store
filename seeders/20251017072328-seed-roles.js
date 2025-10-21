50b4fa7f1d305fce5263aced5f90551d71879b44
antonovmike <antonovma@outlook.com>
2025-10-21 20:32:06 +0200

    [REF] Removed extra comments. Edited README

--- seeders/20251017072328-seed-roles.js
+++ seeders/20251017072328-seed-roles.js
@@ -3,27 +3,11 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     await queryInterface.bulkInsert('Roles', [
       { name: 'admin', createdAt: new Date(), updatedAt: new Date() },
       { name: 'user', createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down (queryInterface, Sequelize) {}
 };
