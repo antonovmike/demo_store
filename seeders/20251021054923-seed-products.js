e17592a2115948f0fe1216e4401bb3cfe593b11a
antonovmike <antonovma@outlook.com>
2025-10-21 07:52:08 +0200

    [ADD] Add seeder to generate seed data for product in database

---
+++ seeders/20251021054923-seed-products.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('Products', [
+      {
+        name: 'Table',
+        price: 99.99,
+        description: 'A sturdy wooden table.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Chair',
+        price: 49.99,
+        description: 'A comfortable chair.',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Products', null, {});
+  },
+};
