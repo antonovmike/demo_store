f0b641bd23d0010c99ad59eb4187267d3c61aa96
antonovmike <antonovma@outlook.com>
2025-11-21 15:50:13 +0100

    [REF] Removed unused React import

--- demo-store-client/src/components/ProfilePage.jsx
+++ demo-store-client/src/components/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import api from "../api/axios";
 import { AuthContext } from '../context/AuthContext';
 
