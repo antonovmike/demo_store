39d48e84404f32000de0585f935f4be5712637e7
antonovmike <antonovma@outlook.com>
2026-01-31 07:48:04 +0100

    [IMP] Add Error type "sequelize" in server/src/init_db.ts

--- server/src/init_db.ts
+++ server/src/init_db.ts
@@ -1,5 +1,6 @@
 import app from "./server.js";
 import initDb from "./db/init.js";
+import { Error } from "sequelize";
 
 const PORT = 1337;
 
@@ -9,7 +10,7 @@ const PORT = 1337;
     app.listen(PORT, () => {
       console.log(`Server running at http://127.0.0.1:${PORT}`);
     });
-  } catch (err) {
+  } catch (err: Error) {
     console.error("Failed to initialize application:", err.message);
     console.error(err.stack);
     process.exit(1); // End the process with error
