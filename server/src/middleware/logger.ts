375dcbf1e035e07e7aab4f7d893a9d72fb211ac7
antonovmike <antonovma@outlook.com>
2026-02-01 09:08:41 +0100

    [REF] Add types in server/src/middleware/logger.ts

--- server/src/middleware/logger.ts
+++ server/src/middleware/logger.ts
@@ -1,6 +1,8 @@
-function logger(req, res, next) {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  }
+import type { Request, Response, NextFunction } from "express";
+
+function logger(req: Request, _res: Response, next: NextFunction) {
+  console.log(`${req.method} ${req.url}`);
+  next();
+}
 
 export default logger;
