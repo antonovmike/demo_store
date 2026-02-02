38092b848a127e7f00d26127a76f2d2448cbc344
antonovmike <antonovma@outlook.com>
2026-02-02 09:14:31 +0100

    [FIX] Add types in server/src/controllers/userController.ts

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,46 +1,51 @@
+import type { Request, Response, NextFunction } from "express";
+
 import userService from "../services/userService.js";
 
-async function getMe(req, res, next) {
+async function getMe(req: Request, res: Response, next: NextFunction) {
   try {
     const user = req.user;
     res.json({ id: user.id, username: user.username, role: user.role });
   } catch (err) {
-    console.error("Error fetching user info:", err.message);
-    next(err);
+    const error = err as Error;
+    console.error("Error fetching user info:", error.message);
+    next(error);
   }
 }
 
-async function register(req, res, next) {
+async function register(req: Request, res: Response, next: NextFunction) {
   try {
     const { username, password, role } = req.body;
     const user = await userService.register(username, password, role);
     res.status(201).json(user);
   } catch (err) {
-    console.error("Error registering user:", err.message);
-    if (err.message === "Username and password are required") {
-      return res.status(400).json({ error: err.message });
+    const error = err as Error;
+    console.error("Error registering user:", error.message);
+    if (error.message === "Username and password are required") {
+      return res.status(400).json({ error: error.message });
     }
-    if (err.message === "User already exists") {
-      return res.status(400).json({ error: err.message });
+    if (error.message === "User already exists") {
+      return res.status(400).json({ error: error.message });
     }
-    next(err);
+    next(error);
   }
 }
 
-async function login(req, res, next) {
+async function login(req: Request, res: Response, next: NextFunction) {
   try {
     const { username, password } = req.body;
     const token = await userService.login(username, password);
     res.json({ token });
   } catch (err) {
-    console.error("Error logging in user:", err.message);
-    if (err.message === "Username and password are required") {
-      return res.status(400).json({ error: err.message });
+    const error = err as Error;
+    console.error("Error logging in user:", error.message);
+    if (error.message === "Username and password are required") {
+      return res.status(400).json({ error: error.message });
     }
-    if (err.message === "User not found or invalid username or password") {
-      return res.status(401).json({ error: err.message });
+    if (error.message === "User not found or invalid username or password") {
+      return res.status(401).json({ error: error.message });
     }
-    next(err);
+    next(error);
   }
 }
 
