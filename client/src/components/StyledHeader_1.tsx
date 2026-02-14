79adb75fdd3f65a27f1bad8cdd11c2d61032bc70
antonovmike <antonovma@outlook.com>
2026-02-14 14:54:27 +0100

    [REF] Fix typing in StyledHeader_1.tsx and StyledPage.tsx
    - Added type React.ReactNode to children

--- client/src/components/StyledHeader_1.tsx
+++ client/src/components/StyledHeader_1.tsx
@@ -1,3 +1,7 @@
-export default function StyledHeader_1({ children }) {
+export default function StyledHeader_1({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return <h1 className={`text-[#8ec07c] font-mono text-xl`}>{children}</h1>;
 }
--- client/src/components/StyledPage.tsx
+++ client/src/components/StyledPage.tsx
@@ -1,4 +1,8 @@
-export default function StyledPage({ children }) {
+export default function StyledPage({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start bg-[#242424] text-white font-sans antialiased">
       {children}
