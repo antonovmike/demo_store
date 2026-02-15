9f3d6df90bbe505162d837f8d544724ad701ecec
antonovmike <antonovma@outlook.com>
2026-02-15 10:34:07 +0100

    [IMP] frontend: Add email and setEmail to client/src/context/AuthContext.ts

--- client/src/context/AuthContext.ts
+++ client/src/context/AuthContext.ts
@@ -5,6 +5,8 @@ import type { User } from "../store/userSlice";
 interface AuthContextType {
   user: User | null;
   setUser: (user: User | null) => void;
+  email: string | null;
+  setEmail: (email: string | null) => void;
   token: string | null;
   setToken: (token: string | null) => void;
   logout: () => void;
