72b32738b3f4d1fe0bbce8e2c0cd39f85f87ddb8
antonovmike <antonovma@outlook.com>
2026-02-19 13:31:59 +0100

    [IMP] Add background, text, typography to client/src/theme.ts

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -2,9 +2,17 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    mode: "light",
-    primary: { main: "#1976d2" },
-    secondary: { main: "#9c27b0" },
+    mode: "dark",
+    background: {
+      default: "#242424",
+      paper: "#242424",
+    },
+    text: {
+      primary: "#ffffff",
+    },
+  },
+  typography: {
+    fontFamily: "sans-serif",
   },
   components: {
     MuiTextField: {
