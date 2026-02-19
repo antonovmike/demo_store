967cb8cd3f657a5c01cca1a9b4d202d59588870e
antonovmike <antonovma@outlook.com>
2026-02-19 14:32:17 +0100

    [REM] Remove client/src/components/StyledHeader_2.tsx

--- client/src/components/StyledHeader_2.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-interface StyledHeaderProps {
-  children: ReactNode;
-}
-
-export default function StyledHeader_2({ children }: StyledHeaderProps) {
-  return <h2 className="text-2xl font-bold mb-4 text-center">{children}</h2>;
-}
