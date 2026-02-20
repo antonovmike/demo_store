2c4431a0b47f15bcd76d1bef03d5c85fda652078
antonovmike <antonovma@outlook.com>
2026-02-20 07:31:35 +0100

    [REM] Remove index.css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import "./index.css";
 
 import App from "./App.jsx";
 import store from "./store/store";
