54aad94ed090b0a7a06f5cf12d483d25d66b7ab7
antonovmike <antonovma@outlook.com>
2026-02-23 21:07:16 +0100

    [REM] Remove import gGrid

--- client/src/components/ProductsPage.tsx
+++ client/src/components/ProductsPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Box, Divider, Grid, Typography } from "@mui/material";
+import { Box, Divider, Typography } from "@mui/material";
 
 import {
   fetchProducts,
