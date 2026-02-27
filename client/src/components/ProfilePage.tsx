43fcd5e918ecb664ce7361dc8f21936f6e00f6b5
antonovmike <antonovma@outlook.com>
2026-02-27 14:00:02 +0100

    [IMP] Add Skeleton loader on ProfilePage.tsx

--- client/src/components/ProfilePage.tsx
+++ client/src/components/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import type { AxiosError } from "axios";
 import { useContext, useEffect, useState } from "react";
-import { Divider, Button, Typography } from "@mui/material";
+import { Divider, Button, Typography, Skeleton } from "@mui/material";
 
 import api from "../api/axios";
 import { AuthContext } from "../context/AuthContext";
@@ -11,6 +11,7 @@ export default function ProfilePage() {
   if (!auth) throw new Error("AuthContext not provided");
   const { token, logout } = auth;
   const [profile, setProfile] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -19,7 +20,11 @@ export default function ProfilePage() {
         const res = await api.get("/users/me", {
           headers: { Authorization: `Bearer ${token}` },
         });
-        console.log("✅ Profile data:", res.data);
+
+        // Check if the 'loading' works and "Loading profile..."
+        // appears after login before profile is loaded
+        // await new Promise((r) => setTimeout(r, 1000));
+
         setProfile(res.data);
       } catch (err) {
         const error = err as AxiosError<{ error: string }>;
@@ -29,6 +34,8 @@ export default function ProfilePage() {
           error.response?.data,
         );
         setProfile(null);
+      } finally {
+        setLoading(false);
       }
     };
     fetchProfile();
@@ -44,7 +51,15 @@ export default function ProfilePage() {
       >
         My Profile
       </Typography>
-      {profile ? (
+      {loading ? (
+        <Divider sx={{ p: 2 }}>
+          {" "}
+          <Typography variant="body1">Loading profile...</Typography>{" "}
+          <Skeleton variant="text" width={200} />{" "}
+          <Skeleton variant="text" width={150} />{" "}
+          <Skeleton variant="rectangular" width={300} height={40} />{" "}
+        </Divider>
+      ) : profile ? (
         <Divider>
           <Typography>
             <strong>ID:</strong> {profile.id}
