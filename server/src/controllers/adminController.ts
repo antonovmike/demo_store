23212df22e2d79c1a530b8f957c004ff1ea5accf
antonovmike <antonovma@outlook.com>
2026-04-07 20:25:57 +0200

    [IMP] Implement logic for function changeUserPassword

--- server/src/controllers/adminController.ts
+++ server/src/controllers/adminController.ts
@@ -1,11 +1,34 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 
-import models from "../models/index.js";
+import { User } from "../models/user.js";
 
-const { User } = models;
+// Admin can change user's password
+async function changeUserPassword(req: Request, res: Response) {
+  try {
+    const {userId, newPassword} = req.body;
 
-async function changeUserPassword() {}
+    if (!userId || !newPassword) {
+      return res.status(400).json({ error: "User ID and new password are required" });
+    }
+    
+    const user = await User.findByPk(userId)
+
+    if (!user) {
+        return res.status(404).json({ error: "User not found" });
+    }
+
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+    user.password_hash = passwordHash;
+    await user.save();
+
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (err) {
+    const error = err as Error;
+    console.error("Error changing user password:", error.message);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
 
 export default {
   changeUserPassword,
